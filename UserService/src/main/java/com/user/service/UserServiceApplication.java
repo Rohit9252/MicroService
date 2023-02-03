89eef19c0889096f5b951c557d52ec41589c008c
Rohit9252 <rohitbatra0165@gmail.com>
2023-02-03 20:41:56 +0530

    'userservice'

--- UserService/src/main/java/com/user/service/UserServiceApplication.java
+++ UserService/src/main/java/com/user/service/UserServiceApplication.java
@@ -1,13 +1,13 @@
-package com.productservice;
+package com.user.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
 }
--- productService/src/main/java/com/productservice/Repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.productservice.Repository;
-
-import com.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-
-}
-
